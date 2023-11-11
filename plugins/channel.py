28a004a6ff1276d22a2047f09b414e7392d405ba
yogeshmirro2 <132906109+yogeshmirro2@users.noreply.github.com>
2023-11-11 17:54:35 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,21 +1,45 @@
 from pyrogram import Client, filters
-
-from info import CHANNELS
-from utils import save_file
+from utils.database import db
 
 media_filter = filters.document | filters.video | filters.audio
 
 
-@Client.on_message(filters.chat(CHANNELS) & media_filter)
+@Client.on_message(filters.channel & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    for file_type in ("document", "video", "audio"):
-        media = getattr(message, file_type, None)
-        if media is not None:
-            break
-    else:
-        return
-
-    media.file_type = file_type
-    media.caption = message.caption
-    await save_file(media)
+    try:
+        if message.chat.id in await db.db_channels_status("get_db_list"):
+            if message.video:
+                channel_id = int(message.chat.id)
+                file_id = message.video.file_id
+                file_unique_id = message.video.file_unique_id
+                file_size = message.video.file_size
+                file_name = message.video.file_name
+                file_caption = message.caption if message.caption else ""
+            
+            elif message.document:
+                channel_id = int(message.chat.id)
+                file_id = message.document.file_id
+                file_unique_id = message.document.file_unique_id
+                file_size = message.document.file_size
+                file_name = message.document.file_name
+                file_caption = message.caption if message.caption else ""
+            
+            elif message.audio:
+                channel_id = int(message.chat.id)
+                file_id = message.audio.file_id
+                file_unique_id = message.audio.file_unique_id
+                file_size = message.audio.file_size
+                file_name = message.audio.file_name
+                file_caption = message.caption if message.caption else ""
+            
+            else return
+            
+            await db.save_media(channel_id,file_id,file_unique_id,file_size,file_name,file_caption)
+        
+        else:
+            await bot.leave_chat(message.chat.id)
+            return
+    except Exception as e:
+        await bot.send_message(Config.BOT_OWNER,f"error during add media to database when media arrive in channel\n{e}\n{e.__class__.__name__}\nError From :- `{__file__,e.__traceback__.tb_lineno}`")
+    
