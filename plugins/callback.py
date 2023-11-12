f891fb499105f1026bbb10a815ef19fa2164b28e
yogeshmirro2 <132906109+yogeshmirro2@users.noreply.github.com>
2023-11-12 09:45:43 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -10,115 +10,138 @@ async def button(bot:Client, cmd:CallbackQuery):
     cb_data = cmd.data
     
     if "next" in cb_data:
-        unused_var, query, next_page, total_pages = cb_data.split("_")
-        if int(next_page)>int(total_pages):
-            next_page = 1
-        files, total_results, total_pages, current_page = await db.get_search_results(query, current_page=int(next_page))
-        if len(files)>=1:
-            btn = []
-            for result in files:
-                result_unique_id = result.get('file_unique_id')
-                result_caption = result.get("file_caption")
-                result_size = result.get("file_size")
-                result_size = await get_file_size(result_size)
-                btn.append([InlineKeyboardButton(f"{result_size}:{result_caption}", callback_data=f"send_{result_unique_id}")])
-            #adding next ,back , close & page No. button
-            btn.append([InlineKeyboardButton("BACK",callback_data=f"back_{query}_{current_page-1}_{total_pages}"),InlineKeyboardButton(f"{current_page}/{total_pages}",callback_data="ignore"),InlineKeyboardButton("NEXT",callback_data=f"next_{query}_{current_page+1}_{total_pages}")])
-            btn.append([InlineKeyboardButton("Close", callback_data="close")])
-            await cmd.message.edit(f"**{total_results}** Result Found for **__{query}__**",reply_markup = InlineKeyboardMarkup(btn))
-        else:
+        try:
+            unused_var, query, next_page, total_pages = cb_data.split("_")
+            if int(next_page)>int(total_pages):
+                next_page = 1
+            files, total_results, total_pages, current_page = await db.get_search_results(query, current_page=int(next_page))
+            if len(files)>=1:
+                btn = []
+                for result in files:
+                    result_unique_id = result.get('file_unique_id')
+                    result_caption = result.get("file_caption")
+                    result_size = result.get("file_size")
+                    result_size = await get_file_size(result_size)
+                    btn.append([InlineKeyboardButton(f"{result_size}:{result_caption}", callback_data=f"send_{result_unique_id}")])
+                #adding next ,back , close & page No. button
+                btn.append([InlineKeyboardButton("BACK",callback_data=f"back_{query}_{current_page-1}_{total_pages}"),InlineKeyboardButton(f"{current_page}/{total_pages}",callback_data="ignore"),InlineKeyboardButton("NEXT",callback_data=f"next_{query}_{current_page+1}_{total_pages}")])
+                btn.append([InlineKeyboardButton("Close", callback_data="close")])
+                await cmd.message.edit(f"**{total_results}** Result Found for **__{query}__**",reply_markup = InlineKeyboardMarkup(btn))
+            else:
+                return
+        
+        except Exception as e:
+            await cmd.message.edit(f"somthing went wrong\nError - {e}\nError Type - `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",quote=True)
             return
-    
-    
     
     elif "back" in cb_data:
-        unused_var, query, next_page, total_pages = cb_data.split("_")
-        if int(next_page)==0:
-            next_page = int(total_pages)
-        files, total_results, total_pages, current_page = await db.get_search_results(query, current_page=int(next_page))
-        if len(files)>=1:
-            btn = []
-            for result in files:
-                result_unique_id = result.get('file_unique_id')
-                result_caption = result.get("file_caption")
-                result_size = result.get("file_size")
-                result_size = await get_file_size(result_size)
-                btn.append([InlineKeyboardButton(f"{result_size}:{result_caption}", callback_data=f"send_{result_unique_id}")])
-            #adding next ,back , close & page No. button
-            btn.append([InlineKeyboardButton("BACK",callback_data=f"back_{query}_{current_page-1}_{total_pages}"),InlineKeyboardButton(f"{current_page}/{total_pages}",callback_data="ignore"),InlineKeyboardButton("NEXT",callback_data=f"next_{query}_{current_page+1}_{total_pages}")])
-            btn.append([InlineKeyboardButton("Close", callback_data="close")])
-            await cmd.message.edit(f"**{total_results}** Result Found for **__{query}__**",reply_markup = InlineKeyboardMarkup(btn))
-        else:
+        try:
+            unused_var, query, next_page, total_pages = cb_data.split("_")
+            if int(next_page)==0:
+                next_page = int(total_pages)
+            files, total_results, total_pages, current_page = await db.get_search_results(query, current_page=int(next_page))
+            if len(files)>=1:
+                btn = []
+                for result in files:
+                    result_unique_id = result.get('file_unique_id')
+                    result_caption = result.get("file_caption")
+                    result_size = result.get("file_size")
+                    result_size = await get_file_size(result_size)
+                    btn.append([InlineKeyboardButton(f"{result_size}:{result_caption}", callback_data=f"send_{result_unique_id}")])
+                #adding next ,back , close & page No. button
+                btn.append([InlineKeyboardButton("BACK",callback_data=f"back_{query}_{current_page-1}_{total_pages}"),InlineKeyboardButton(f"{current_page}/{total_pages}",callback_data="ignore"),InlineKeyboardButton("NEXT",callback_data=f"next_{query}_{current_page+1}_{total_pages}")])
+                btn.append([InlineKeyboardButton("Close", callback_data="close")])
+                await cmd.message.edit(f"**{total_results}** Result Found for **__{query}__**",reply_markup = InlineKeyboardMarkup(btn))
+            else:
+                return
+        except Exception as e:
+            await cmd.message.edit(f"somthing went wrong\nError - {e}\nError Type - `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",quote=True)
             return
-    
     
     elif "send" in cb_data:
-        response = await verify_before_send(bot,cmd)
-        if response == 20:
-            file_unique_id = cb_data.split("_",1)[-1]
-            file_id , file_caption = await db.get_file(file_unique_id)#cb_data.split("_")[-1] get file_unique_id
-            await bot.send_cached_media(cmd.from_user.id,file_id,file_caption)
-            await cmd.answer()
+        try:
+            response = await verify_before_send(bot,cmd)
+            if response == 20:
+                file_unique_id = cb_data.split("_",1)[-1]
+                file_id , file_caption = await db.get_file(file_unique_id)#cb_data.split("_")[-1] get file_unique_id
+                await bot.send_cached_media(cmd.from_user.id,file_id,file_caption)
+                await cmd.answer()
+                return
             return
-        return
-
+        except Exception as e:
+            await cmd.message.edit(f"somthing went wrong\nError - {e}\nError Type - `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",quote=True)
+            return
+    
     elif "refreshForceSub" in cb_data:
-        updates_channel = await db.update_channel_status("get_id")
-        update_channel_link = await db.update_channel_link_status("get_link")
-        if (updates_channel and updates_channel_link) is not None:
-            try:
-                user = await bot.get_chat_member(updates_channel, cmd.from_user.id)
-                if user.status == "kicked":
+        try:
+            updates_channel = await db.update_channel_status("get_id")
+            update_channel_link = await db.update_channel_link_status("get_link")
+            if (updates_channel and updates_channel_link) is not None:
+                try:
+                    user = await bot.get_chat_member(updates_channel, cmd.from_user.id)
+                    if user.status == "kicked":
+                        await cmd.message.edit(
+                            text="Sorry Sir, You are Banned to use me. Contact my Support.",
+                            disable_web_page_preview=True
+                        )
+                        return
+                except UserNotParticipant:
                     await cmd.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my Support.",
+                        text="**You Still Didn't Join ☹️, Please Join My Updates Channel to use this Bot!**\n\n"
+                             "Due to Overload, Only Channel Subscribers can use the Bot!",
+                        reply_markup=InlineKeyboardMarkup(
+                            [
+                                [
+                                    InlineKeyboardButton("🤖 Join Updates Channel", url=update_channel_link)
+                                ],
+                                [
+                                    InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
+                                ]
+                            ]
+                        )
+                    )
+                    return
+                except Exception:
+                    await cmd.message.edit(
+                        text=f"Something went Wrong. Contact my Support.\nError⚠️:`{e}`\nError Type➡️ `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",
                         disable_web_page_preview=True
                     )
                     return
-            except UserNotParticipant:
-                await cmd.message.edit(
-                    text="**You Still Didn't Join ☹️, Please Join My Updates Channel to use this Bot!**\n\n"
-                         "Due to Overload, Only Channel Subscribers can use the Bot!",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("🤖 Join Updates Channel", url=update_channel_link)
-                            ],
-                            [
-                                InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
-                            ]
-                        ]
-                    )
-                )
-                return
+            await cmd.message.edit(
+                text=f"**Hi! I'm Movie/Webserver search bot\nHere you can search movie/webseries name with correct spelling**\nExample :- `/search Avengers`\n\nप्रिय यूजर! मैं एक simple movie/webseries सर्च bot हूं।आप कोई भी movie/webseries सर्च कर सकते है , अगर वह मेरे database में होगी तो आपके भेज दी जाएगी" ,
+                
+            )
+        except Exception as e:
+            await cmd.message.edit(f"somthing went wrong\nError - {e}\nError Type - `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",quote=True)
+            return
+    
+    
+    elif "verification" in cb_data:
+        try:
+            
+            bool_string = cb_data.rsplit("_",1)[-1]
+            try:
+                await db.verification_status(bool_string)
             except Exception:
                 await cmd.message.edit(
                     text=f"Something went Wrong. Contact my Support.\nError⚠️:`{e}`\nError Type➡️ `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",
                     disable_web_page_preview=True
                 )
                 return
-        await cmd.message.edit(
-            text=f"**Hi! I'm Movie/Webserver search bot\nHere you can search movie/webseries name with correct spelling**\nExample :- `/search Avengers`\n\nप्रिय यूजर! मैं एक simple movie/webseries सर्च bot हूं।आप कोई भी movie/webseries सर्च कर सकते है , अगर वह मेरे database में होगी तो आपके भेज दी जाएगी" ,
-            
-        )
-    
-    
-    elif "verification" in cb_data:
-        bool_string = cb_data.rsplit("_",1)[-1]
-        try:
-            await db.verification_status(bool_string)
-        except Exception:
-            await cmd.message.edit(
-                text=f"Something went Wrong. Contact my Support.\nError⚠️:`{e}`\nError Type➡️ `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",
-                disable_web_page_preview=True
-            )
+            await cmd.message.edit(f"**Now your verification is {bool_string}**")
+        except Exception as e:
+            await cmd.message.edit(f"somthing went wrong\nError - {e}\nError Type - `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",quote=True)
             return
-        await cmd.message.edit(f"**Now your verification is {bool_string}**")
-
+    
     elif "use_pre_shorted_link" in cb_data:
-        bool_string = cb_data.rsplit("_",1)[-1]
-        await db.use_pre_shorted_link_status(bool_string)
-        await cmd.message.edit(f"**Now your use_pre_shorted_link is {bool_string}**")
-
+        try:
+            bool_string = cb_data.rsplit("_",1)[-1]
+            await db.use_pre_shorted_link_status(bool_string)
+            await cmd.message.edit(f"**Now your use_pre_shorted_link is {bool_string}**")
+        except Exception as e:
+            await cmd.message.edit(f"somthing went wrong\nError - {e}\nError Type - `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",quote=True)
+            return
+    
     
     
     
@@ -126,8 +149,12 @@ async def button(bot:Client, cmd:CallbackQuery):
     
     
     elif "close" in cb_data:
-        await cmd.message.delete(True)
-        
+        try:
+            await cmd.message.delete(True)
+        except Exception as e:
+            await cmd.message.edit(f"somthing went wrong\nError - {e}\nError Type - `{e.__class__.__name__}`\nError From :- `{__file__,e.__traceback__.tb_lineno}`",quote=True)
+            return
+    
         
     try:
         await cmd.answer()
